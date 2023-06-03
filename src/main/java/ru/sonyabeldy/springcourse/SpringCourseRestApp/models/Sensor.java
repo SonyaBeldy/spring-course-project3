5fe20b9ebf2ffed7b06181fded66b316eb464939
Sonya <sofya.beldy@mail.ru>
2023-06-03 23:06:10 +1000

    create Sensor entity

---
+++ src/main/java/ru/sonyabeldy/springcourse/SpringCourseRestApp/models/Sensor.java
@@ -0,0 +1,40 @@
+package ru.sonyabeldy.springcourse.SpringCourseRestApp.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Sensor")
+public class Sensor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+
+    @Column(name = "name")
+    String name;
+
+    public Sensor() {
+    }
+
+    public Sensor(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
